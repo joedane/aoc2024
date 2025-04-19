8bbf43778353b334dcc637067ef8e02814316d86
Joseph Dane <joe.dane@gmail.com>
2025-04-18 15:07:51 -1000

    day14 wip

--- src/bin/d14.rs
+++ src/bin/d14.rs
@@ -1,3 +1,4 @@
+use raylib::prelude::*;
 use std::{collections::HashMap, str::FromStr};
 
 use regex::Regex;
@@ -42,7 +43,8 @@ impl FromStr for Bot {
         ))
     }
 }
-fn main() {
+
+fn part1() {
     let input = std::fs::read_to_string("input/d14.txt").unwrap();
     //let input = TEST;
     let (width, height) = (101_i16, 103_i16);
@@ -88,6 +90,61 @@ fn main() {
         }
     }
     println!("{}", map.values().fold(1, |acc, v| acc * v));
+}
+
+fn move_bots(bots: &mut Vec<Bot>, width: i16, height: i16) {
+    bots.iter_mut().for_each(|b| {
+        b.p.x = (b.p.x + b.v.x).rem_euclid(width);
+        b.p.y = (b.p.y + b.v.y).rem_euclid(height);
+    });
+}
+
+fn part2() {
+    let (mut rl, thread) = raylib::init().size(1000, 1000).build();
+    rl.set_target_fps(20);
+
+    let (x_base, y_base) = (35, 35);
+    let (cell_width, cell_height) = (9, 9);
+    let (x_offset, y_offset) = (1, 1);
+    let (rect_width, rect_height) = (7, 7);
+    let mut last_update: f64 = 0.0;
+    let update_freq: f64 = 0.1;
+
+    let input = std::fs::read_to_string("input/d14.txt").unwrap();
+    //let input = TEST;
+    let (width, height) = (101_i16, 103_i16);
+    let mut frame = 0;
+    let mut bots: Vec<Bot> = input
+        .lines()
+        .map(str::trim)
+        .map(|s| s.parse().unwrap())
+        .collect();
+
+    while !rl.window_should_close() {
+        let current_time = rl.get_time();
+        if last_update + update_freq < current_time {
+            move_bots(&mut bots, width, height);
+            last_update = current_time;
+            frame += 1;
+        }
+        rl.draw(&thread, |mut d| {
+            d.clear_background(Color::WHITE);
+            d.draw_text(&format!("Frame: {frame}"), 12, 12, 20, Color::BLACK);
+            for bot in bots.iter() {
+                d.draw_rectangle(
+                    bot.p.x as i32 * cell_width + x_offset + x_base,
+                    bot.p.y as i32 * cell_height + y_offset + y_base,
+                    rect_width,
+                    rect_height,
+                    Color::RED,
+                )
+            }
+        });
+    }
+}
+
+fn main() {
+    part2();
 }
 
 static TEST: &str = "p=0,4 v=3,-3
