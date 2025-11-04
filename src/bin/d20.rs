95e19d3f18c187086e120c852e86f9493022c6d7
Joe Dane <joe.dane@gmail.com>
2025-11-03 19:29:03 -1000

    d20, before another change of strategy

--- src/bin/d20.rs
+++ src/bin/d20.rs
@@ -3,6 +3,7 @@ use std::{
     sync::Arc,
 };
 
+use itertools::Itertools;
 use utils::{BasicGrid, Coord, Dir};
 
 #[derive(Debug, Clone, Copy)]
@@ -109,10 +110,15 @@ fn trace_path(grid: &BasicGrid<GridPoint>) -> (Vec<Coord>, HashMap<Coord, usize>
 
 fn possible_jumps<T>(grid: &BasicGrid<T>, from: Coord, dist: usize) -> Vec<Coord> {
     let mut targets: HashSet<Coord> = Default::default();
+    let mut visited: HashSet<Coord> = Default::default();
     let mut to_visit: Vec<(Coord, usize)> = Default::default();
     to_visit.push((from, dist));
     while let Some((c, dist)) = to_visit.pop() {
         targets.insert(c);
+
+        if c.row == 3 && c.col == 1 {
+            println!("here we are");
+        }
         if dist > 0 {
             for d in [Dir::Up, Dir::Left, Dir::Down, Dir::Right] {
                 if let Some(pos) = grid.next_pos(c, d) {
@@ -127,7 +133,7 @@ fn possible_jumps<T>(grid: &BasicGrid<T>, from: Coord, dist: usize) -> Vec<Coord
     targets.into_iter().collect()
 }
 
-fn process(data: String, dist: usize, min_savings: usize) {
+fn process_part1(data: String, dist: usize, min_savings: usize) {
     let mut input: Vec<&str> = vec![];
     for line in data.lines().map(str::trim) {
         input.push(line);
@@ -168,12 +174,60 @@ fn process(data: String, dist: usize, min_savings: usize) {
     println!("{}", save_hash.iter().map(|(k, v)| v).sum::<usize>());
 }
 
+fn manhattan_len(c1: Coord, c2: Coord) -> usize {
+    c1.col.abs_diff(c2.col) + c1.row.abs_diff(c2.row)
+}
+
+fn process_part2(data: String, dist: usize, min_savings: usize) {
+    let mut input: Vec<&str> = vec![];
+    for line in data.lines().map(str::trim) {
+        input.push(line);
+    }
+    let grid: BasicGrid<GridPoint> = BasicGrid::new(&input);
+
+    let (path, steps) = trace_path(&grid);
+    let path_len = path.len();
+    for p in &path {
+        println!("{:?} => {}", p, steps.get(p).unwrap());
+    }
+    let mut save_hash: HashMap<usize, usize> = Default::default();
+
+    println!("path is {} long", path.len());
+    for p in &path {
+        let this_step = *steps.get(p).unwrap();
+        for pos in possible_jumps(&grid, *p, dist) {
+            if let Some(&step) = steps.get(&pos) {
+                if step > this_step + manhattan_len(p.clone(), pos) {
+                    let savings = step - this_step - manhattan_len(p.clone(), pos);
+                    if savings >= min_savings {
+                        save_hash
+                            .entry(savings)
+                            .and_modify(|c| *c += 1)
+                            .or_insert(1);
+
+                        println!(
+                            "jumping from pos {:?} to {:?} will save {} steps",
+                            p, pos, savings
+                        );
+                    }
+                }
+            }
+        }
+    }
+
+    println!("{:?}", save_hash.len());
+    println!("{:?}", save_hash);
+    for s in save_hash.into_iter().sorted_by(|s1, s2| s1.0.cmp(&s2.0)) {
+        println!("There are {:>2} cheats that save {} picoseconds.", s.1, s.0);
+    }
+}
+
 fn part1(data: String) {
-    process(data, 2, 100);
+    process_part1(data, 2, 100);
 }
 
 fn part2(data: String) {
-    process(data, 20, 50);
+    process_part2(data, 20, 50);
 }
 
 fn main() {
@@ -207,7 +261,23 @@ mod test {
     #[test]
     fn test_jumps() {
         let g: BasicGrid<u16> = BasicGrid::new_default(15, 15);
-        let v = super::possible_jumps(&g, Coord::new(1, 1), 3);
-        println!("{:?}", v);
+        let start = Coord::new(1, 1);
+        let v = super::possible_jumps(&g, start, 20);
+        for r in 0..g.height {
+            for c in 0..g.width {
+                let this_c = Coord::new(r, c);
+                print!(
+                    "{}",
+                    if this_c == start {
+                        "O"
+                    } else if v.contains(&this_c) {
+                        "x"
+                    } else {
+                        "."
+                    }
+                );
+            }
+            println!();
+        }
     }
 }
